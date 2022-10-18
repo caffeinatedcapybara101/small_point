d9e93c6b313502c92ab1aa5217ddba899a986d3a
caffeinatedcapybara101 <angela_xing+cs1300@brown.edu>
2022-10-18 16:10:49 -0400

    about and hiring pages

--- nav_bar.js
+++ nav_bar.js
@@ -4,11 +4,42 @@
 /* Open when someone clicks on the menu element */
 function openNav() {
     document.getElementById("navbar").style.width = "100%";
-    document.getElementsByClassName("slideshow")[0].style.display = "none";
+    let slideshow = document.getElementsByClassName("slideshow");
+    if (slideshow.length != 0) {
+        slideshow[0].style.display = "none";
+    }
+    let pageImg = document.getElementsByClassName("page-img")
+    if (pageImg.length != 0) {
+        pageImg[0].style.display = "none";
+    }
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
     document.getElementById("navbar").style.width = "0%";
-    document.getElementsByClassName("slideshow")[0].style.display = "flex";
+    let slideshow = document.getElementsByClassName("slideshow");
+    if (slideshow.length != 0) {
+        slideshow[0].style.display = "flex";
+    }
+    let pageImg = document.getElementsByClassName("page-img")
+    if (pageImg.length != 0) {
+        pageImg[0].style.display = "flex";
+    }
+}
+
+window.onresize = function () {
+    if (window.innerWidth < 770) {
+        document.getElementById("navbar").style.width = "0%";
+    }
+    if (window.innerWidth > 770) {
+        document.getElementById("navbar").style.width = "100%"
+        let slideshow = document.getElementsByClassName("slideshow");
+        if (slideshow.length != 0) {
+            slideshow[0].style.display = "flex";
+        }
+        let pageImg = document.getElementsByClassName("page-img")
+        if (pageImg.length != 0) {
+            pageImg[0].style.display = "flex";
+        }
+    }
 }
